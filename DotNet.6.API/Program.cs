d6c47022cdc20d16b20063e3be8c45534df5c874
Pradipkumar Desai <pdesai.programming@gmail.com>
2023-06-07 22:08:36 +0530

    Change return type

--- DotNet.6.API/Program.cs
+++ DotNet.6.API/Program.cs
@@ -8,7 +8,11 @@ namespace DotNet._6.API
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers(options =>
+            {
+                options.ReturnHttpNotAcceptable = true;
+            }).AddXmlDataContractSerializerFormatters();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
