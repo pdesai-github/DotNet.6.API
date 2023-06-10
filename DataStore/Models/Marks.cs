5cca223ec7eb07ea6d44c2bc05197e3763acc6e3
Pradipkumar Desai <pdesai.programming@gmail.com>
2023-06-10 17:34:18 +0530

    EF 6 added

---
+++ DataStore/Models/Marks.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStore.Models
+{
+    public class Marks
+    {
+        public Marks(string subject, int score)
+        {
+            Subject = subject;
+            Score = score;
+        }
+
+        public string Subject { get; set; }        
+        public int Score { get; set; }
+
+        public int Id { get; set; }
+
+        public Marks()
+        {
+            Subject = string.Empty;
+            Score = 0;
+        }
+
+    }
+}
